3c4a2128420599fc58a82b74045b5b08fee51363
fyunka <glitchfishzero@gmail.com>
2019-06-15 19:46:03 +0500

    Initial Commit

---
+++ Whats The Weather/ViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ViewController.swift
+//  Whats The Weather
+//
+//  Created by sophia on 6/15/19.
+//  Copyright © 2019 fyunka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var cityTextField: UITextField!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    @IBAction func getWeather(_ sender: Any) {
+        
+        if let url = URL(string: "https://www.weather-forecast.com/locations/" + cityTextField.text!.replacingOccurrences(of: " ", with: "-") + "/forecasts/latest") {
+        
+        let request = NSMutableURLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            
+            data, response, error in
+            
+            var message = ""
+            
+            if error != nil {
+                
+                print(error as Any)
+                
+            } else {
+                
+                if let unwrappedData = data {
+                    
+                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                    
+                    var stringSeparator = "<p class=\"b-forecast__table-description-content\"><span class=\"phrase\">"
+                    
+                    if let contentArray = dataString?.components(separatedBy: stringSeparator) {
+                        
+                        if contentArray.count > 1 {
+                            
+                            stringSeparator = "</span>"
+                            
+                            let newContentArray = contentArray[1].components(separatedBy: stringSeparator)
+                            
+                            if newContentArray.count > 1 {
+                                
+                                message = newContentArray[0].replacingOccurrences(of: "&deg;", with: "°")
+                                
+                                print(message)
+                                
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            
+            if message == "" {
+                
+                message = "The weather there couldn't be found. Please try again."
+                
+            }
+            
+            
+            DispatchQueue.main.sync(execute: {
+          
+                self.resultLabel.text = message
+                
+            })
+            
+        }
+        
+        task.resume()
+            
+        } else {
+            
+            resultLabel.text = "The weather there couldn't be found. Please try again."
+            
+            
+            
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+    
+
